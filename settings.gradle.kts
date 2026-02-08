120b8ecb2a493002c00ca0360f6eadcc4ae615b7
Дима Ш. <dima.o.sh@proton.me>
2026-02-08 20:31:51 +0000

    Add plugin repositories to settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,12 @@
+pluginManagement {
+    repositories {
+        maven("https://maven.fabricmc.net/")
+
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
 plugins {
     // Apply the foojay-resolver plugin to allow automatic download of JDKs
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
