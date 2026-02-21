792161f3fcfe5e8a825905eb689e30af236173f3
Дима Ш. <dima.o.sh@proton.me>
2026-02-21 14:39:18 +0000

    Update mod logger name
    Updated the name of the logger to be consistent with other loggers.

--- src/main/kotlin/org/example/template/TemplateMod.kt
+++ src/main/kotlin/org/example/template/TemplateMod.kt
@@ -1,13 +1,18 @@
 package org.example.template
 
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.loader.api.FabricLoader
+import net.fabricmc.loader.api.ModContainer
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 object TemplateMod : ModInitializer {
 	const val MOD_ID = "template"
 
-	val logger: Logger = LoggerFactory.getLogger(MOD_ID)
+	val container: ModContainer = FabricLoader.getInstance()
+		.getModContainer(MOD_ID)
+		.orElseThrow()
+	val logger: Logger = LoggerFactory.getLogger(container.metadata.name.replace(" ", ""))
 
 	override fun onInitialize() {
 		logger.info("Hello Fabric world!")
