9d804d87ac3750897318e9180fd94ddbd7df77ac
Дима Ш. <dima.o.sh@proton.me>
2026-07-24 23:56:56 +0100

    Remove unneeded Gradle plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,9 +7,4 @@ pluginManagement {
 	}
 }
 
-plugins {
-	// Apply the foojay-resolver plugin to allow automatic download of JDKs
-	id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-}
-
 rootProject.name = "template"
